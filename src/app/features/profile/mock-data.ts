f7b11ce139626ab0442f68fabab869c91a811a43
AyazhanTaimas <123255704+AyazhanTaimas@users.noreply.github.com>
2024-12-21 17:31:55 +0500

    Add files via upload

---
+++ src/app/features/profile/mock-data.ts
@@ -0,0 +1,8 @@
+import { Profile } from './models/profile.model';
+
+export const mockUser: Profile = {
+  username: 'mockuser',
+  bio: 'This is a mock user profile for testing purposes.',
+  image: 'https://example.com/mock-user-image.jpg',
+  following: false,
+};
